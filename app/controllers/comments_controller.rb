58c90706d05a15ace2a227c0033e2cf9f10fc154
kobahumi <mattyaminto@gmail.com>
2020-12-01 23:27:52 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+
+  def index
+    @comment = Comment.new
+    @prototype = Prototype.find(params[:prototype_id])
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to prototype_path(@comment.prototype)
+    else
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
+      render "prototypes/show"
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+end
